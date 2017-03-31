002bc0c1e1cce9daa11c3ce766be596b584448a1
Guillermo Garrido <G6In3D@users.noreply.github.com>
2017-03-31 16:02:00 -0400

    Update geometry.py

--- geometry.py
+++ geometry.py
@@ -1,1 +1,26 @@
+import math
 
+def lawOfCosines(B, C, a):
+    rhs = B**2 + C**2 - 2*B*C*math.cos(a*math.pi/180)
+    return math.sqrt(rhs)
+
+def lawOfSines(A, a, B):
+    rhs = B*math.sin(a*math.pi/180)/A
+    return math.asin(rhs)*180/math.pi
+
+print "Enter side B:"
+B = float(raw_input())
+
+print "Enter side C:"
+C = float(raw_input())
+
+print "Enter angle a:"
+a = float(raw_input())
+
+A = lawOfCosines(B, C, a)
+
+b = lawOfSines(A, a, B)
+
+c = lawOfSines(A, a, C)
+
+print A, b, c
